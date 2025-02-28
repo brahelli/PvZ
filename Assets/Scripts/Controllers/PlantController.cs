72ed0b6e2e02b83fce093090129bfe3a9e1c04cb
Brahelli <bhe0611@gmail.com>
2025-02-28 10:28:59 +0000

    Code cleanup

--- Assets/Scripts/Controllers/PlantController.cs
+++ Assets/Scripts/Controllers/PlantController.cs
@@ -17,94 +17,10 @@ public class PlantController : MonoBehaviour
     
     //Initialise and assign variables
 
-    private void Update()
-    {
-        //CheckForZombies();
-        //Check for zombies in the raycast origins
-    }
-
-    /*private void CheckForZombies()
-    {
-        Vector2 checkDir = Vector2.zero;
-
-        switch (player)
-        {
-            case 1:
-                checkDir = Vector2.left;
-                break;
-            case 2:
-                checkDir = Vector2.right;
-                break;
-            case 3:
-                checkDir = Vector2.up;
-                break;
-            case 4:
-                checkDir = Vector2.down;
-                break;
-        }
-        
-        for (int i = 0; i < raycastOrigins.Length; i++)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(raycastOrigins[i], checkDir, 100f, zombies);
-            //For every raycast origin, check if there is a zombie in the way
-
-            if (hit.collider && hit.collider.CompareTag("Zombie"))
-            {
-                //If there is a zombie in the way, set the tag of the plant row to ZombieDetected
-                switch (i)
-                {
-                    case 0:
-                        plantRowOne.tag = "ZombieDetected";
-                        break;
-                    case 1:
-                        plantRowTwo.tag = "ZombieDetected";
-                        break;
-                    case 2:
-                        plantRowThree.tag = "ZombieDetected";
-                        break;
-                    case 3:
-                        plantRowFour.tag = "ZombieDetected";
-                        break;
-                    case 4:
-                        plantRowFive.tag = "ZombieDetected";
-                        break;
-                }
-            }
-            else
-            {
-                //If there is no zombie in the way, set the tag of the plant row to Untagged
-                switch (i)
-                {
-                    case 0:
-                        plantRowOne.tag = "Untagged";
-                        break;
-                    case 1:
-                        plantRowTwo.tag = "Untagged";
-                        break;
-                    case 2:
-                        plantRowThree.tag = "Untagged";
-                        break;
-                    case 3:
-                        plantRowFour.tag = "Untagged";
-                        break;
-                    case 4:
-                        plantRowFive.tag = "Untagged";
-                        break;
-                }
-            }
-        }
-    }*/
-
     private void StartSpawn(string plantType, int sunCost, int playerS)
     {
         //If the player does not have enough sun, return and do nothing
         if (!(gameManager.sun >= sunCost)) return;
-        
-        /*if (!_isSpawning)
-        {
-            gameManager.sun -= sunCost;
-            //Subtract the cost of the plant from the player's sun if the player is not trying to spawn a plant already
-        }*/
         
         gameManager.sun -= sunCost;
 
