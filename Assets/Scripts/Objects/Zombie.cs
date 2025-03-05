54852b3c99292af5fc025ded96848a215f66542d
Brahelli <bhe0611@gmail.com>
2025-03-05 11:57:08 +0000

    Zombies can Attack other players

--- Assets/Scripts/Objects/Zombie.cs
+++ Assets/Scripts/Objects/Zombie.cs
@@ -12,17 +12,65 @@ public class Zombie : MonoBehaviour
     private Rigidbody2D _rb;
 
     private Animator _anim;
+    
+    private GameManager _gameManager;
+    
+    private Vector2 _direction;
+    
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         
+        int randomDirection = 0;
+
+        switch (_gameManager.noOfPlayers)
+        {
+            case 1:
+                randomDirection = 1;
+                break;
+            case 2:
+                randomDirection = Random.Range(1, 3);
+                break;
+            case 3:
+                randomDirection = Random.Range(1, 4);
+                break;
+            case 4:
+                randomDirection = Random.Range(1, 5);
+                break;
+        }
+
+        switch (randomDirection)
+        {
+            case 1:
+                transform.rotation = Quaternion.identity;
+                spriteRenderer.flipX = false;
+                _direction = Vector2.left;
+                break;
+            case 2:
+                transform.rotation = Quaternion.identity;
+                spriteRenderer.flipX = true;
+                _direction = Vector2.right;
+                break;
+            case 3:
+                transform.rotation = Quaternion.Euler(0, 0, 270);
+                spriteRenderer.flipX = false;
+                _direction = Vector2.up;
+                break;
+            case 4:
+                transform.rotation = Quaternion.Euler(0, 0, 270);
+                spriteRenderer.flipX = true;
+                _direction = Vector2.down;
+                break;
+        }
     }
 
     private void Update()
     {
-        _rb.MovePosition(_rb.position + Vector2.left * (moveSpeed * Time.deltaTime));
+        _rb.MovePosition(_rb.position + _direction * (moveSpeed * Time.deltaTime));
 
         if (health <= 0) { Die(); }
     }
